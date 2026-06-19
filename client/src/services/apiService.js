79c2856e11b66cb156ec991debc6695d06be0bc1
smurzzz <faldashenry25@gmail.com>
2026-06-19 11:03:58 +0800

    fix apiService to use config

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../config/api';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5002/api',
+  baseURL: API_BASE_URL,
 });
 
 apiClient.interceptors.request.use((config) => {
